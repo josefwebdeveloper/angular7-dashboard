b0c0f8160f23aad43630a3190524645716ef002b
Josef Shainskiy <josef2011@gmail.com>
2018-10-31 00:02:30 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+  window.console.log = () => {
+      };
   enableProdMode();
 }
 
